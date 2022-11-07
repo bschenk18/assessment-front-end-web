46ffd27ff04a43969bfa6824da8d07e240330700
Benjamin Prentiss <bschenk18@yahoo.com>
2022-11-07 11:19:43 -0700

    assessment-front-end-web

---
+++ profile.js
@@ -0,0 +1,17 @@
+// const colorBtn = document.querySelector(`#color`)
+// const placeBtn = document.querySelector(`#place`)
+// const ritualBtn = document.querySelector(`#ritual`)
+
+// const colorClick = () => {
+//     alert(`Yellow`)
+// }
+// const placeClick = () => {
+//     alert(`Disney Land`)
+// }
+// const ritualClick = () => {
+//     alert(`Squeezing my rubber ducky`)
+// }
+
+// colorBtn.addEventListener(`click`, colorClick)
+// placeBtn.addEventListener(`click`, placeClick)
+// ritualBtn.addEventListener(`click`, ritualClick)
